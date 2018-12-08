bb4372f6231fc19161940f8a5975a0a4dff57f30
Jim Schlader <jamesschlader@yahoo.com>
2018-12-08 13:36:14 -0700

    another server adjustment for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var PORT = 3000;
+var PORT =  process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
